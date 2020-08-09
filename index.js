4e6455f788dbccd2d273c0f3cb7d646f6b13eca6
Jordy Pena <jordypenamontes@gmail.com>
2020-08-08 23:32:13 -0500

    youtube api added

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+const APIKEY = `AIzaSyDZEqC-1amZMJiATBCGnpxarIpT_eBmzR0`
+const VIDEO = ""
 //create event listener for which dropdown bodypart was selected
+//fetch body part and amount of workouts needed
+//store data into a function to use for displaying
 $("#submit").on("click", event => {
     $("#result").empty();
-    console.log("bodypart")
+    
     event.preventDefault();
     let selected = $("#bodypart").val();
     let amount = $("#resultAmount").val();
-    fetch(`https://wger.de/api/v2/exerciseinfo/`)
-    .then(response => response.json())
+    fetch(`https://wger.de/api/v2/exerciseinfo/?language=2&status=2`)
+    .then(response => { 
+        if (response.status = 200)
+            return response.json() 
+        else if (response.status = 404) 
+            alert("No results found please select a bodypart from the dropdown menu.")
+        else 
+            alert("Something went wrong please try again.")
+        
+    })
     .then(responseJson => {
-        console.log(responseJson)
+        
         let counter = 0; 
         for (let i = 0; i < responseJson.length; i++) {
             
@@ -18,41 +30,64 @@ $("#submit").on("click", event => {
 
                     if (responseJson[i].description !== "") {
 
-                        categoryDisplay(responseJson[i].description);
+                        categoryDisplay(responseJson[i]);
                         counter++;
-                        console.log("hello")
+                        
                     }
 
                 }
-                
-                    
-               //console.log(counter); 
             }
         }
         
     })
+    .catch(error => alert(error)) 
+    videoSearch(APIKEY,selected,5)
 })
+//display workout name
+//dispay workout description
+function categoryDisplay(data) {
+    let myHmtl = 
+    `<div class="workout-name">${data.name}</div>
+    <div class="workout-description">${data.description}</div>` 
+    
+    $("#result").append(myHmtl);
 
-function categoryDisplay(description) {
-    $("#result").append(description);
-
-
+    
 }
-//then fetch data with that bodyparts id
-
- //this is to store excersise id that user selected
-
-//fetch the actual workout based on category id
-//fetch(`https://wger.de/api/v2/exercise/?format=json&exercise=${id}`)
-//.then(response => response.json())
-//.then(responseJson => {
-
-//})
-    //create a catch for if 200 ok and 404 when no matches
-    //display
 
 
 // run youtube only when you get the workout API working!!
+//fetch youtube api
+//grab video and display
+
+function videoSearch(key, userSelection, maxResults) {
+    $("#videos").empty()
+    fetch(`https://www.googleapis.com/youtube/v3/search?key=${APIKEY}&type=video&part=snippet&maxResults=${maxResults}&q=${userSelection}+workout`)
+    .then(response => {
+        if (response.status = 200)
+            return response.json()
+        else if (response.status = 404)
+            alert("No results found please select a bodypart from the dropdown menu.")
+        else
+            alert("Something went wrong please try again.")
+    })
+    .then(responseJson => {
+        console.log(responseJson)
+        
+       for (let i = 0; i < responseJson.items.length; i++)  {
+                     
+                 let VIDEO = `<iframe width="300" height="225" src="http://www.youtube.com/embed/${responseJson.items[i].id.videoId}" frameborder="0" allowfullscreen></iframe>`
+
+                    $("#videos").append(VIDEO);
+        }
+    }) 
+    .catch(error => alert(error)) 
+    
+}
+
+
+
+
 
 
 //feedback from riley remember to have a function to generate information
